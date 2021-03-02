982297b328e40ae767ff2d73168268307ed5bbce
tanykazy <tanykazy@gmail.com>
2021-03-02 23:45:39 +0900

    Add fadein() fadeout()

--- src/app/components/youtube-player/youtube-player.component.ts
+++ src/app/components/youtube-player/youtube-player.component.ts
@@ -12,11 +12,18 @@ export class YoutubePlayerComponent implements OnInit {
 
   @Output() ended = new EventEmitter();
 
-  videoId: string | undefined;
+  _videoId: string | undefined;
+  set videoId(id: string) {
+    this._videoId = id;
+    this.youtube.playVideo();
+    this.fadein();
+  }
+
   height: number | undefined;
   width: number | undefined;
   startSeconds: number | undefined;
   endSeconds: number | undefined;
+  baseVolume: number;
   // suggestedQuality: "default" | "small" | "medium" | "large" | "hd720" | "hd1080" | "highres" | undefined;
   // showBeforeIframeApiLoads: boolean | undefined;
 
@@ -28,6 +35,7 @@ export class YoutubePlayerComponent implements OnInit {
     // this.endSeconds = 8;
     // this.suggestedQuality = "highres";
     // this.showBeforeIframeApiLoads = true;
+    this.baseVolume = 100;
   }
 
   ngOnInit(): void {
@@ -43,20 +51,66 @@ export class YoutubePlayerComponent implements OnInit {
 
     // for auto play when first time loaded
     event.target.playVideo();
+    this.fadein();
   }
 
   onStateChange(event: any): void {
     console.log(event);
 
-    // for auto play when second time loaded
-    if (event.data === 5) {
-      event.target.playVideo();
-    }
-
     if (event.data === 0) {
       this.ended.emit(event.data);
+    } else if (event.data === 2) {
+      event.target.playVideo();
+      this.fadein();
+    } else if (event.data === 5) {
+      // for auto play when second time loaded
+      event.target.playVideo();
+      this.fadein();
     }
+  }
 
+  /**
+   * skip
+   */
+  public skip() {
+
+  }
+
+  /**
+   * fadein
+   */
+  public fadein() {
+    const volume = this.youtube.getVolume();
+    if (volume > 0) {
+      this.youtube.setVolume(0);
+      this.baseVolume = volume;
+    }
+    const step = Math.floor(this.baseVolume / 10);
+    const timerid = setInterval(() => {
+      const volume = this.youtube.getVolume();
+      if (volume < this.baseVolume) {
+        this.youtube.setVolume(volume + step);
+      } else {
+        clearInterval(timerid);
+      }
+    }, 200);
+  }
+
+  /**
+   * fadeout
+   */
+  public fadeout() {
+    this.baseVolume = this.youtube.getVolume();
+    const step = Math.floor(this.baseVolume / 10);
+    const timerid = setInterval(() => {
+      const volume = this.youtube.getVolume();
+      if (volume > 0) {
+        this.youtube.setVolume(volume - step);
+      } else {
+        clearInterval(timerid);
+        this.youtube.pauseVideo();
+      }
+    }, 200);
   }
 
   /**
