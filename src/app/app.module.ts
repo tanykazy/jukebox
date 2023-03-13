cac16edc6151c5a3a147e7b3936ef295d4b549b8
tanykazy <tanykazy@gmail.com>
2023-03-13 14:16:32 +0900

    Add @angular/pwa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -23,6 +23,7 @@ import { AppComponent } from './app.component';
 import { YoutubePlayerComponent } from './components/youtube-player/youtube-player.component';
 import { RequestBoxComponent } from './components/request-box/request-box.component';
 import { RequestDialogComponent } from './components/request-dialog/request-dialog.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [
@@ -51,6 +52,12 @@ import { RequestDialogComponent } from './components/request-dialog/request-dial
     MatListModule,
     MatExpansionModule,
     DragDropModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent],
